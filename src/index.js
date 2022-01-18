ec968dc7d3303c240caebb84b50b9d449400776c
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-01-18 08:34:31 -0500

    completed assignment, no db.json(will not commit?)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", () => {  
+  let form=document.querySelector("#form")
+  form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    buildToDo(e.target.task.value)
+    form.reset()
+  })
 });
+
+
+function buildToDo(todo){
+  console.log(todo)
+  let p=document.createElement('p')
+  let btn=document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent="x"
+  p.textContent=todo
+  p.appendChild(btn)
+  document.querySelector("#form").appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
+
